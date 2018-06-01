c624a6523bbc642bc1a4253d7b25466c3b1c79a2
azrdev <azrdev@qrdn.de>
2018-06-01 15:38:07 +0200

    tests: run sklearn testsuite one at a time
    so we can see which fail and how, instead of one single big log

--- sklearn_seco/tests/test_seco.py
+++ sklearn_seco/tests/test_seco.py
@@ -1,9 +1,17 @@
-from sklearn.utils.estimator_checks import check_estimator
+from sklearn.utils.estimator_checks import check_estimator, _yield_all_checks
 from sklearn_seco.seco_base import SimpleSeCoEstimator, CN2Estimator
 
 
 def test_check_simple():
-    return check_estimator(SimpleSeCoEstimator)
+    # return check_estimator(SimpleSeCoEstimator)
+
+    # disassembled check_estimator, to have all as separate nosetests
+    name = SimpleSeCoEstimator.__name__
+    estimator = SimpleSeCoEstimator()
+    for check in _yield_all_checks(name, estimator):
+        check.description = check.__name__
+        yield check, name, estimator
+
 
 
 def test_check_CN2():
