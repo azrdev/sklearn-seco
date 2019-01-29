f9a816fd87eaa5f1dda4cac8c35ed24772a7f040
azrdev <azrdev@qrdn.de>
2019-01-29 15:01:24 +0100

    extra.py: make coverage tracing compatible with common.py changes
    i.e. split into SeCoBaseImplementation,RuleContext,TheoryContext

--- sklearn_seco/tests/test_extra.py
+++ sklearn_seco/tests/test_extra.py
@@ -1,46 +1,39 @@
 """Tests for `sklearn_seco.extra`."""
 
-from numpy.testing import assert_array_equal
+from collections import namedtuple
 
-from sklearn_seco.abstract import SeCoEstimator, _BinarySeCoEstimator
+from sklearn_seco.abstract import _BinarySeCoEstimator
 from sklearn_seco.extra import trace_coverage, plot_coverage_log
 from sklearn_seco.tests.datasets import binary_categorical
 
 
-def test_coverage_tracing(seco_estimator):
+def test_coverage_tracing(seco_estimator_class):
     """Test the `trace_coverage` mixin, using SimpleSeCo and CN2."""
 
-    implementation_class = type(seco_estimator.implementation)
+    Trace = namedtuple("Trace", ["coverage_log", "refinement_log", "last_rule_stop", "PN"])
+    trace: Trace = None
 
-    TracingImpl = trace_coverage(implementation_class)
-    tracer = TracingImpl()
-    estimator = SeCoEstimator(tracer)
+    def trace_callback(coverage_log, refinement_log, last_rule_stop, PN):
+        nonlocal trace
+        assert trace is None  # only one run & estimator for binary problem
+        trace = Trace(coverage_log, refinement_log, last_rule_stop, PN)
+
+    estimator = trace_coverage(seco_estimator_class, trace_callback)()
     X, y, X_test, y_test, cm = binary_categorical()
     estimator.fit(X, y, categorical_features=cm)
     # check recognition of binary problem
     base = estimator.base_estimator_
     assert isinstance(base, _BinarySeCoEstimator)
     # check consistency of trace
-    assert tracer.has_complete_trace
-    assert isinstance(tracer.last_rule_stop, bool)
+    assert isinstance(trace, Trace)
+    assert isinstance(trace.last_rule_stop, bool)
     # subtract boolean, autocasts to integer 0/1
-    assert len(base.theory_) == len(tracer.coverage_log) - tracer.last_rule_stop
-    assert len(base.theory_) == len(tracer.refinement_log) - tracer.last_rule_stop
+    assert len(base.theory_) == len(trace.coverage_log) - trace.last_rule_stop
+    assert len(base.theory_) == len(trace.refinement_log) - trace.last_rule_stop
     # TODO trace levels
 
-    # test (de)serialization
-    json_str = tracer.to_json()
-    trace_recovered = tracer.from_json(json_str)
-    assert tracer.last_rule_stop == trace_recovered["last_rule_stop"]
-    for x, y in zip(tracer.coverage_log, trace_recovered["coverage_log"]):
-        assert_array_equal(x, y)
-    for x, y in zip(tracer.refinement_log, trace_recovered["refinement_log"]):
-        assert_array_equal(x, y)
-    for x, y in zip(tracer.PN, trace_recovered["PN"]):
-        assert_array_equal(x, y)
-
     # test plotting
-    tf, rfs = plot_coverage_log(**trace_recovered,
-                                title=implementation_class.__name__)
+    tf, rfs = plot_coverage_log(**trace._asdict(),
+                                title=seco_estimator_class.__name__)
     tf.show()
     rfs.show()
