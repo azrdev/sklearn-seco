c6055ac7607feb828980d048d8e2915f7e42adae
azrdev <azrdev@qrdn.de>
2019-05-09 21:34:50 +0200

    make profile-test.py work after multiclass and test/Dataset changes

--- profile-test.py
+++ profile-test.py
@@ -1,10 +1,9 @@
-import sys
 import warnings
 
 from line_profiler import LineProfiler
 
 from sklearn_seco.tests.conftest import xor_2d
-from sklearn_seco.common import match_rule, count_matches
+from sklearn_seco.common import match_rule, RuleContext
 from sklearn_seco.concrete import CN2Estimator
 
 
@@ -12,19 +11,18 @@ def tcn2():
     with warnings.catch_warnings():
         from _pytest.deprecated import RemovedInPytest4Warning
         warnings.simplefilter("ignore", RemovedInPytest4Warning)
-        x, y, xtest, ytest = xor_2d()
+        xor = xor_2d()
     cn2 = CN2Estimator()
-    cn2.fit(x, y)
-    ypred = cn2.predict(xtest)
+    cn2.fit(xor.x_train, xor.y_train)
+    ypred = cn2.predict(xor.x_test)
     from sklearn.metrics import classification_report, confusion_matrix
-    print(confusion_matrix(ytest, ypred))
-    print(classification_report(ytest, ypred))
+    print(confusion_matrix(xor.y_test, ypred))
+    print(classification_report(xor.y_test, ypred))
 
 
 profile = LineProfiler()
-if 'm' in sys.argv[1:]:
-    profile.add_function(match_rule)
-if 'c' in sys.argv[1:]:
-    profile.add_function(count_matches)
+profile.add_function(match_rule)
+profile.add_function(RuleContext._count_matches)
+profile.add_function(RuleContext.pn)
 profile.run('tcn2()')
 profile.print_stats()
